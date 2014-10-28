574390fae413ea1150bb9c393b0d7b44a00ce1f2
App Academy Student <student@appacademy.io>
2014-10-28 15:59:15 -0400

    add bio new or edit partial for main page to always say Edit settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ KickStarter::Application.routes.draw do
   resources :users do
     resources :user_bio, only: [:new]
   end
-  resources :user_bio, only: [:create, :edit, :destroy]
+  resources :user_bio, only: [:create, :edit, :update, :destroy]
   resource :session
 end
