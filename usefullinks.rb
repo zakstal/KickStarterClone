50ee99ee0c17b035f7a563b216f41015706a2e07
App Academy Student <student@appacademy.io>
2014-10-30 20:44:39 -0400

    double check push

---
+++ usefullinks.rb
@@ -0,0 +1,8 @@
+---useful links---
+
+http://cocoapods.org/
+http://blog.filepicker.io/post/45899334267/filepicker-io-zencoder-adding-video-to-your-app-in
+https://app.zencoder.com/login
+https://developers.filepicker.io/services/
+https://github.com/Ink/filepicker-rails
+https://github.com/tduek/ninjas/blob/master/config/routes.rb
