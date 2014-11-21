029869121023776930fcb57979d0067d2e62ce1f
App Academy Student <appacademy@Apps-Mac-mini-8.local>
2014-11-21 17:11:43 -0500

    add

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  # before_filter :require_logged_in
+  def new
+    @user = Users.new
+  end
+
+  def create
+    @user = Users.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      log_in?(@user)
+      # redirect_after_require_login_or(user_url(@user))
+      redirect_to root_url
+    else
+      @user = Users.new
+      flash.now[:erros] = ['Invalid username or password']
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
